8e89712a3b10224e59ddb876eaf5a14973c42cb2
djfordz <djfordz@gmail.com>
2014-08-27 10:40:27 -0500

    added comments to build.js / removed block borders

--- build.js
+++ build.js
@@ -1,30 +1,50 @@
+// initialize script
 function clone() {
+
+// Set global variables
     var canvas = document.getElementById('canvas'), innerCanvas = document.getElementById('innerCanvas'),initialBlock = document.createElement('div'), log = document.getElementById('log');
+
+// build initial block
     innerCanvas.appendChild(initialBlock);
     initialBlock.setAttribute('class', 'block');
     initialBlock.setAttribute('id', '0');
-  //  innerCanvas.setAttribute('width', '960');
-    var total = window.innerHeight * 3;
 
-     function totalBlocks() {
-        var totalBlocks = innerCanvas.childNodes.length;
-        return totalBlocks;
+// set initial width of #innerCanvas
+    innerCanvas.style.width = innerCanvasWidth(64);
+
+// Set area of #innerCanvas
+    function innerCanvasArea() {
+        var total = parseInt(innerCanvas.style.width) * 2;
+        return total;
     }
 
-    while(totalBlocks() <= total) {
+// Set total blocks
+     function totalBlocks() {
+        var totalBlocks = innerCanvas.childNodes.length ;
+        return totalBlocks + 1;
+    }
+
+// populate blocks
+    while(totalBlocks() <= innerCanvasArea()) {
         var block = initialBlock.cloneNode(true);
         innerCanvas.appendChild(block)
      
     }
+
+// set attribute #id to all blocks populated above in order from 0
         for(var i = 1, l = innerCanvas.childNodes.length - 1; i <= l; i++) {
             document.getElementsByClassName('block')[i].setAttribute('id', i)
         }
 
+// change color property to camelCase to use in Javascript if needed and to ensure cross-browser integration with Microsoft IE.  copied from: http://www.sophox.com/wordpress/?p=764
     function hyphen2camel (str) {
         return str.replace(/\-(.)/g, function (m, c) { 
         return c.toUpperCase();
         } );
     }
+
+// Method to pull the CSS Property from an external stylesheet.
+// Modified code to include identifying and element by class as well as id.  Original code copied from: http://www.sophox.com/wordpress/?p=759.
     function computeStyle(el, property) {
         var computedStyle, val = false
         if(document.getElementById(el) && window.getComputedStyle) {
@@ -47,49 +67,64 @@ function clone() {
         }
        return val;
     }
+
+// compute #canvas width
     function canvasWidth() {
          var canvasWidth = computeStyle('canvas', 'width');
         canvasWidth = parseInt(fillDivWidth);
         return canvasWidth;
     }
 
+// compute block width
     function blockWidth() {
         var blockWidth = computeStyle('block', 'width');
         blockWidth = parseInt(blockWidth);
         return blockWidth;
     }
+
+//compute number of blocks in #innerCanvas
     function innerCanvasWidth(width) {
         var innerCanvasWidth = width;
         var numBlocks = innerCanvasWidth * blockWidth();
         return numBlocks + 'px'
     }
+
+// log which block the mouse pointer is hovering over
      function position(pos) {
         log.innerHTML = pos
     }
 
+// create onmouseover method
     canvas.onmouseover = function(x) {
+        // get block type mouse is over
         var blockType = x.target;
+        //get block #id mouse is over
         var pos = x.target.id;
+        // change background color to red of block mouse is over
        if(document.getElementById(pos).hasAttribute('class')) {
             blockType.style.backgroundColor = 'red'
         }
+        // call position function to log block mouse is coloring
         position(pos)
     }
 
+// reset #innerCanvas
     function reset() {
         var button = document.getElementById('reset');
         button.onclick = function() {
+            // cycle through blocks resetting background color to white.
              for(var i = 0, l = innerCanvas.childNodes.length - 1; i <= l; i++) {
                 var block = document.getElementsByClassName('block')[i];
                 if(block.hasAttribute('class')) {
                 block.style.backgroundColor = 'white';
                 }
             }
+            // prompt for width of #innerCanvas
             var width = prompt("Enter width of canvas in number of squares");
              innerCanvas.style.width = innerCanvasWidth(width);
-             position(innerCanvas.style.width)
         }
     }
+// call reset function
     reset();
 
 };
