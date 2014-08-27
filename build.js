cf0ebab7a091a023aeb5cc3ba98957e03460c483
djfordz <djfordz@gmail.com>
2014-08-27 11:21:20 -0500

    fixed git add/add error by removing the code added by git

--- build.js
+++ build.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // initialize script
 function clone() {
 
@@ -27,50 +26,25 @@ function clone() {
 
 // populate blocks
     while(totalBlocks() <= innerCanvasArea()) {
-=======
-function clone() {
-    var canvas = document.getElementById('canvas'), innerCanvas = document.getElementById('innerCanvas'),initialBlock = document.createElement('div'), log = document.getElementById('log');
-    innerCanvas.appendChild(initialBlock);
-    initialBlock.setAttribute('class', 'block');
-    initialBlock.setAttribute('id', '0');
-  //  innerCanvas.setAttribute('width', '960');
-    var total = window.innerHeight * 3;
-
-     function totalBlocks() {
-        var totalBlocks = innerCanvas.childNodes.length;
-        return totalBlocks;
-    }
-
-    while(totalBlocks() <= total) {
->>>>>>> Setup GithUb Pages
         var block = initialBlock.cloneNode(true);
         innerCanvas.appendChild(block)
      
     }
-<<<<<<< HEAD
 
 // set attribute #id to all blocks populated above in order from 0
-=======
->>>>>>> Setup GithUb Pages
         for(var i = 1, l = innerCanvas.childNodes.length - 1; i <= l; i++) {
             document.getElementsByClassName('block')[i].setAttribute('id', i)
         }
 
-<<<<<<< HEAD
 // change color property to camelCase to use in Javascript if needed and to ensure cross-browser integration with Microsoft IE.  copied from: http://www.sophox.com/wordpress/?p=764
-=======
->>>>>>> Setup GithUb Pages
     function hyphen2camel (str) {
         return str.replace(/\-(.)/g, function (m, c) { 
         return c.toUpperCase();
         } );
     }
-<<<<<<< HEAD
 
 // Method to pull the CSS Property from an external stylesheet.
 // Modified code to include identifying and element by class as well as id.  Original code copied from: http://www.sophox.com/wordpress/?p=759.
-=======
->>>>>>> Setup GithUb Pages
     function computeStyle(el, property) {
         var computedStyle, val = false
         if(document.getElementById(el) && window.getComputedStyle) {
@@ -93,46 +67,33 @@ function clone() {
         }
        return val;
     }
-<<<<<<< HEAD
 
 // compute #canvas width
-=======
->>>>>>> Setup GithUb Pages
     function canvasWidth() {
          var canvasWidth = computeStyle('canvas', 'width');
         canvasWidth = parseInt(fillDivWidth);
         return canvasWidth;
     }
 
-<<<<<<< HEAD
 // compute block width
-=======
->>>>>>> Setup GithUb Pages
     function blockWidth() {
         var blockWidth = computeStyle('block', 'width');
         blockWidth = parseInt(blockWidth);
         return blockWidth;
     }
-<<<<<<< HEAD
 
 //compute number of blocks in #innerCanvas
-=======
->>>>>>> Setup GithUb Pages
     function innerCanvasWidth(width) {
         var innerCanvasWidth = width;
         var numBlocks = innerCanvasWidth * blockWidth();
         return numBlocks + 'px'
     }
-<<<<<<< HEAD
 
 // log which block the mouse pointer is hovering over
-=======
->>>>>>> Setup GithUb Pages
      function position(pos) {
         log.innerHTML = pos
     }
 
-<<<<<<< HEAD
 // create onmouseover method
     canvas.onmouseover = function(x) {
         // get block type mouse is over
@@ -152,40 +113,18 @@ function clone() {
         var button = document.getElementById('reset');
         button.onclick = function() {
             // cycle through blocks resetting background color to white.
-=======
-    canvas.onmouseover = function(x) {
-        var blockType = x.target;
-        var pos = x.target.id;
-       if(document.getElementById(pos).hasAttribute('class')) {
-            blockType.style.backgroundColor = 'red'
-        }
-        position(pos)
-    }
-
-    function reset() {
-        var button = document.getElementById('reset');
-        button.onclick = function() {
->>>>>>> Setup GithUb Pages
              for(var i = 0, l = innerCanvas.childNodes.length - 1; i <= l; i++) {
                 var block = document.getElementsByClassName('block')[i];
                 if(block.hasAttribute('class')) {
                 block.style.backgroundColor = 'white';
                 }
             }
-<<<<<<< HEAD
             // prompt for width of #innerCanvas
             var width = prompt("Enter width of canvas in number of squares");
              innerCanvas.style.width = innerCanvasWidth(width);
         }
     }
 // call reset function
-=======
-            var width = prompt("Enter width of canvas in number of squares");
-             innerCanvas.style.width = innerCanvasWidth(width);
-             position(innerCanvas.style.width)
-        }
-    }
->>>>>>> Setup GithUb Pages
     reset();
 
 };
